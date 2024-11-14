26551021a533004324c4bcf4ed9fc5cd1bab4d95
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-11-14 13:40:58 -0800

    reverted change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pos-landing-page/",
-  // base: "/",
+  // base: "/pos-landing-page/",
+  base: "/",
 });
